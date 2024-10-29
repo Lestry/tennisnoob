de75bb8cc0b28a65f2fa640c8f5e8239da068748
zhangc13 <zhangc13@mingyuanyun.com>
2024-10-29 22:34:54 +0800

    实现部分计分逻辑

--- wxmp/tennis-pointer/miniprogram/pages/index/index.ts
+++ wxmp/tennis-pointer/miniprogram/pages/index/index.ts
@@ -4,16 +4,18 @@ const app = getApp<IAppOption>()
 
 Component({
   data: {
+    // 局分展示文案
+    GAME_POINT_SHOW: ['0', '15', '30', '40', 'AD'],
     // 比赛设置-是否锁定（比赛开始计分时设置为true）
     matchConfig_lock: false,
     // 比赛设置-是否有占先
     matchConfig_hasAd: false,
-    // 比赛设置-每盘X局获胜
-    matchConfig_setGames: 6,
     // 比赛设置-决胜局开始于X平
     matchConfig_tieBreakStart: 6,
-    // 比赛设置-决胜局X分获胜
-    matchConfig_tieBreakPoint: 7,
+    // 是否比赛结束
+    isMatchEnd: false,
+    // 是否决胜局
+    isTieBreak: false,
     // 选手左：名称、局分、盘分
     playerLeft_name: '',
     playerLeft_gamePoint: 0,
@@ -24,41 +26,90 @@ Component({
     playerRight_setPoint: 0,
   },
   methods: {
-    // 改变分数
-    changePoint(player: 'playerLeft' | 'playerRight', type: 'gamePoint' | 'setPoint', point: number) {
-      console.log(player, type, point);
-    },
     // 左侧选手的局分-1
     handleSubstractPlayerLeftGamePoint() {
-      this.changePoint('playerLeft', 'gamePoint', -1);
+      if (this.data.playerLeft_gamePoint > 0) {
+        this.setData({
+          playerLeft_gamePoint: this.data.playerLeft_gamePoint - 1
+        })
+      }
     },
     // 左侧选手的局分+1
     handleAddPlayerLeftGamePoint() {
-      this.changePoint('playerLeft', 'gamePoint', +1);
+      const { matchConfig_hasAd, isTieBreak, playerLeft_gamePoint, playerRight_gamePoint } = this.data;
+      const _newPoint = playerLeft_gamePoint + 1;
+      if (isTieBreak) {
+        // 决胜局中可以一直增加
+        this.setData({
+          playerLeft_gamePoint: _newPoint
+        });
+      } else if (matchConfig_hasAd) {
+        // 有占先的情况下，需要看对手分数
+
+      }
     },
     // 右侧选手的局分-1
     handleSubstractPlayerRightGamePoint() {
-      this.changePoint('playerRight', 'gamePoint', -1);
+      if (this.data.playerRight_gamePoint > 0) {
+        this.setData({
+          playerLeft_gamePoint: this.data.playerRight_gamePoint - 1
+        })
+      }
     },
     // 右侧选手的局分+1
     handleAddPlayerRightGamePoint() {
-      this.changePoint('playerRight', 'gamePoint', +1);
+      console.log('playerRight', 'gamePoint', +1);
     },
     // 左侧选手的盘分-1
     handleSubstractPlayerLeftSetPoint() {
-      this.changePoint('playerLeft', 'setPoint', -1);
+      console.log('playerLeft', 'setPoint', -1);
     },
     // 左侧选手的盘分+1
     handleAddPlayerLeftSetPoint() {
-      this.changePoint('playerLeft', 'setPoint', +1);
+      console.log('playerLeft', 'setPoint', +1);
     },
     // 右侧选手的盘分-1
     handleSubstractPlayerRightSetPoint() {
-      this.changePoint('playerRight', 'setPoint', -1);
+      console.log('playerRight', 'setPoint', -1);
     },
     // 右侧选手的盘分+1
     handleAddPlayerRightSetPoint() {
-      this.changePoint('playerRight', 'setPoint', +1);
+      console.log('playerRight', 'setPoint', +1);
+    },
+    // 比赛结束
+    handleEnd() {
+      const { isTieBreak, playerLeft_gamePoint, playerRight_gamePoint, playerLeft_setPoint, playerRight_setPoint } = this.data;
+      if (isTieBreak) {
+        // 决胜局分数只要不相等就允许结束
+        if (playerLeft_gamePoint === playerRight_gamePoint) {
+          wx.showToast({
+            title: '决胜局小分相同不允许结束比赛',
+            icon: 'none',
+          });
+        } else if (playerLeft_gamePoint > playerRight_gamePoint) {
+          this.setData({
+            isMatchEnd: true,
+            playerLeft_setPoint: playerLeft_setPoint + 1,
+          });
+        } else {
+          this.setData({
+            isMatchEnd: true,
+            playerRight_setPoint: playerRight_setPoint + 1,
+          });
+        }
+      } else if (playerLeft_setPoint === playerRight_setPoint) {
+        wx.showToast({
+          title: '大分相同不允许结束比赛',
+          icon: 'none',
+        });
+      } else {
+        // 非决胜局分出胜负 小分清0显示
+        this.setData({
+          isMatchEnd: true,
+          playerLeft_gamePoint: 0,
+          playerRight_gamePoint: 0,
+        });
+      }
     },
     // 重置比分并解锁比赛设置
     handleReset() {
@@ -71,6 +122,8 @@ Component({
           if (confirm) {
             this.setData({
               matchConfig_lock: false,
+              isTieBreak: false,
+              isMatchEnd: false,
               playerLeft_gamePoint: 0,
               playerLeft_setPoint: 0,
               playerRight_gamePoint: 0,
